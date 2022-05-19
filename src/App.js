74393eb262f3627cb56380c1eb3ced7835369397
jacobnathanson <jacobnath93@gmail.com>
2022-05-19 14:35:25 -0400

    basic app structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+
 function App() {
   return (
     <div className="App">
+      <h1>Need help writing a product description?</h1>
+      <p>Enter some infoation about your product in the form bellow and we'll write you a great description using OpenAI's GPT-3 Model</p>
 
+      <div>form</div>
+
+      <div>results</div>
     </div>
+
+
   );
 }
 
---
+++ src/components/form-pages/ResponsePage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ResponsePage() {
+    return (
+        <div>ResponsePage</div>
+    )
+}
