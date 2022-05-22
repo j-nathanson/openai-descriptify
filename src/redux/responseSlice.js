478cd63e98f63146a9565fbf09d55f9f6413a0d6
jacobnathanson <jacobnath93@gmail.com>
2022-05-21 22:39:38 -0400

    removed sample response from initalState

--- src/redux/responseSlice.js
+++ src/redux/responseSlice.js
@@ -2,20 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     isLoading: false,
-    totalResponses: [
-        {
-            response: 'The Donner Dub-1 acoustic electric bass ukelele is perfect for musicians, hobbyists, and bass players. It has a mahogany finish and built-in tuner, making it easy to play. The weight of 3 lbs makes it portable and compact, while the rubber strings provide great sound.',
-            prompt: {
-                basicDescription: 'A bass ukelele',
-                productName: 'Donner DUB-1 30 inch Acoustic Electric Bass Ukulele',
-                idealUsers: 'Musicians, hobbyists, bass players',
-                features: 'built-in tuner, built-in pick-up, mahogany finish, weight 3 lbs, rubber strings',
-                benefits: 'portable, compact size, affordable, easy to play, and unique, great sound',
-                engine: 'text-curie-001'
-
-            }
-        }
-    ]
+    totalResponses: []
 }
 
 export const responseSlice = createSlice({
